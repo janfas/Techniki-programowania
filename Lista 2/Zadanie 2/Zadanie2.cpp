7e2930f059d4bcd306fbadf4f5d2af3aa7da6830
janfas <jan.fasciszewski@gmail.com>
2020-04-10 14:13:44 +0200

    3.3

--- Lista 2/Zadanie 2/Zadanie2.cpp
+++ Lista 2/Zadanie 2/Zadanie2.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 #include <vector>
 
+void wypisanie(std::vector< std::vector<int> > wypisanie, int n)
+{
+    std::cout << "Tabliczka mnozenia liczb od 1 do " << n << " prezentuje sie nastepujaco:" << std::endl;
+        for(int i=0; i<n; i++)
+        {
+            for(int j=0; j<n; j++)
+            {
+                std::cout.width(10);
+                std::cout << wypisanie[i][j];
+            }
+            std::cout << std::endl;
+        }
+}
+
+void tabliczkaMnozenia(int n)
+{
+    std::vector< std::vector<int> > tabliczkaMnozenia;
+        tabliczkaMnozenia.resize(n);
+        for(int i=0; i<n; i++)
+        {
+            tabliczkaMnozenia.at(i).resize(n);
+            for(int j=0; j<n; j++)
+            {
+                tabliczkaMnozenia.at(i).at(j)=(i+1)*(j+1);
+            }
+        }
+    wypisanie(tabliczkaMnozenia, n);
+}
+
 int main()
 {
     std::cout << "Generowanie tabliczki mnozenia:" << std::endl;
@@ -11,30 +40,13 @@ int main()
     if(n<1)
     {
         std::cout << "Podano zla liczbe!" << std::endl;
+       
         return 0;
     }
     else
     {
-        std::vector< std::vector<int> > tabliczkaMnozenia;
-        tabliczkaMnozenia.resize(n);
-        for(int i=0; i<n; i++)
-        {
-            tabliczkaMnozenia.at(i).resize(n);
-            for(int j=0; j<n; j++)
-            {
-                tabliczkaMnozenia.at(i).at(j)=(i+1)*(j+1);
-            }
-        }
-        std::cout << "Tabliczka mnozenia liczb od 1 do " << n << " prezentuje sie nastepujaco:" << std::endl;
-        for(int i=0; i<n; i++)
-        {
-            for(int j=0; j<n; j++)
-            {
-                std::cout.width(10);
-                std::cout << tabliczkaMnozenia[i][j];
-            }
-            std::cout << std::endl;
-        }
+        tabliczkaMnozenia(n);
+
         return 0;
     }
     
